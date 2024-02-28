505eb6779400ec876b47b2de90c76341fe6da487
RayaXraya <rrayanmr2003@gmail.com>
2024-02-28 05:25:05 -0800

    2nd

---
+++ main.h
@@ -0,0 +1,14 @@
+#ifndef main_H
+#define main_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <limits.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/wait.h>
+
+extern char** environ;
+
+#endif
